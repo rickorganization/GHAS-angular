45e0ab0f352e836896a025aae638001e955ef273
ABJANANI <91468768+ABJANANI@users.noreply.github.com>
2022-07-10 20:04:55 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AccountsComponent } from './accounts/accounts.component';
-import { LoansComponent } from './loans/loans.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { OffersComponent } from './offers/offers.component';
+
 import { CustomPipe } from './custom.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AccountsComponent,
-    LoansComponent,
-    SignupComponent,
-    LoginComponent,
-    OffersComponent,
+  
     CustomPipe,
 
   ],
