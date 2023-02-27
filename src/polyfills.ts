4b3f2261dd1b37dd2b72d7af1e715fdde88d177a
Naveenmorty <naveenkumar1297g@gmail.com>
2023-02-27 18:11:40 +0530

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,7 +17,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
+e34d4d27471827163b423b8f795b307a
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
